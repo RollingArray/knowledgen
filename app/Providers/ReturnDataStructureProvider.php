5368e87f14066f3ccd3a9f28ea817063a331c72f
Rolling Array <82917132+RollingArray@users.noreply.github.com>
2022-01-20 10:15:05 +0530

    Return data structure provider

---
+++ app/Providers/ReturnDataStructureProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use App\Http\Interfaces\ReturnDataStructureServiceInterface;
+use App\Http\Services\ReturnDataStructureService;
+use Illuminate\Support\ServiceProvider;
+
+class ReturnDataStructureProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(
+            ReturnDataStructureServiceInterface::class,
+            ReturnDataStructureService::class
+        );
+    }
+}
