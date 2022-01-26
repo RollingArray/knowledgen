103be15d5eb4d657d6a0523839c1c5286bbc59f1
Rolling Array <82917132+RollingArray@users.noreply.github.com>
2022-01-26 23:30:38 +0530

    Adding article recommendations

---
+++ app/Http/Controllers/CourseMaterialRecommendationController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Interfaces\CourseMaterialServiceInterface;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Http\Interfaces\JWTAuthServiceInterface;
+use App\Http\Interfaces\UsersServiceInterface;
+use App\Models\CourseMaterialModel;
+
+class CourseMaterialRecommendationController extends Controller
+{
+
+	/**
+	 * jwtAuthServiceInterface
+	 *
+	 * @var mixed
+	 */
+	protected $jwtAuthServiceInterface;
+
+	/**
+	 * usersServiceInterface
+	 *
+	 * @var mixed
+	 */
+	protected $usersServiceInterface;
+
+	protected $courseMaterialServiceInterface;
+
+	/**
+	 * __construct
+	 *
+	 */
+	public function __construct(
+		UsersServiceInterface $usersServiceInterface,
+		JWTAuthServiceInterface $jwtAuthServiceInterface,
+		CourseMaterialServiceInterface $courseMaterialServiceInterface
+	) {
+		$this->usersServiceInterface = $usersServiceInterface;
+		$this->jwtAuthServiceInterface = $jwtAuthServiceInterface;
+		$this->courseMaterialServiceInterface = $courseMaterialServiceInterface;
+	}
+
+	/**
+     * rules
+     *
+     * @return void
+     */
+    public function rules()
+    {
+        return [
+            'course_material_name' => 'required|max:255',
+            'course_material_description' => 'required|max:255'
+        ];
+    }
+
+	/**
+	 * custom messages
+	 *
+	 * @return void
+	 */
+	public function customMessages()
+	{
+		return [
+			//
+		];
+	}
+
+	/**
+	 * boot
+	 *
+	 * @param  mixed $request
+	 * @return void
+	 */
+	public function all(Request $request)
+	{
+		$user = $this->usersServiceInterface->getUserById(
+			$request->header('UserId')
+		);
+
+		$data = $this->courseMaterialServiceInterface->findRecommendedCourses($user->user_skills);
+
+		return $this->jwtAuthServiceInterface->sendBackToClient(
+			$request->header('Auth'), 
+			$request->header('UserId'), 
+			'data', 
+			$data
+		);
+	}
+}
