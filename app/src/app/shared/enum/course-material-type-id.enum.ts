17f3acbef0ff4909b5380ed069889d29fd06edc9
Rolling Array <82917132+RollingArray@users.noreply.github.com>
2022-07-05 10:47:12 +0530

    Course material type id enum

---
+++ app/src/app/shared/enum/course-material-type-id.enum.ts
@@ -0,0 +1,22 @@
+/**
+ * © Rolling Array https://rollingarray.co.in/
+ *
+ *
+ * @summary Course material type id enum
+ * @author code@rollingarray.co.in
+ *
+ * Created at     : 2022-06-30 14:49:15 
+ * Last modified  : 2022-07-05 10:46:52
+ */
+
+export enum CourseMaterialTypeIdEnum{
+    TD = "textDocument",
+	QUIZ = "quiz",
+	CW = "crossword",
+	PS = "silds",
+	DC = "dragContent",
+	FC = "flashCard",
+	PL = "poll",
+	PZ = "puzzle",
+	WC = "wordCloud"
+}
