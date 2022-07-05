cf34ff0324099fe8d2e6e74f1965691e3b97e5ee
Rolling Array <82917132+RollingArray@users.noreply.github.com>
2022-07-05 10:52:21 +0530

    Course material type model

---
+++ app/src/app/shared/model/course-material-type.model.ts
@@ -0,0 +1,20 @@
+/**
+ * © Rolling Array https://rollingarray.co.in/
+ *
+ *
+ * @summary Course material type model
+ * @author code@rollingarray.co.in
+ *
+ * Created at     : 2022-06-30 14:23:11 
+ * Last modified  : 2022-07-05 10:52:08
+ */
+
+import { CourseMaterialTypeIdEnum } from "../enum/course-material-type-id.enum";
+
+
+export interface CourseMaterialTypeModel
+{
+	id: CourseMaterialTypeIdEnum;
+	type: string;
+	icon: string;
+}
