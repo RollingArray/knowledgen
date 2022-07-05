44533e9a78aa5eb604ecd396e6acadbd6a164504
Rolling Array <82917132+RollingArray@users.noreply.github.com>
2022-07-05 10:50:10 +0530

    Menu type enum

---
+++ app/src/app/shared/enum/menu-type.enum.ts
@@ -0,0 +1,16 @@
+/**
+ * © Rolling Array https://rollingarray.co.in/
+ *
+ *
+ * @summary Menu type enum
+ * @author code@rollingarray.co.in
+ *
+ * Created at     : 2022-06-30 14:49:15 
+ * Last modified  : 2022-07-05 10:49:50
+ */
+
+export enum MenuTypeEnum{
+    PARENT_MENU = "parentMenu",
+	CHILD_MENU = "childMenu",
+	SUB_CHILD_MENU = "subChildMenu",
+}
