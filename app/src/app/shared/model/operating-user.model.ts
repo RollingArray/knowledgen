96d8f7c3e448873ee9b45ccb7ad7cd5fbcb7a8d7
Rolling Array <82917132+RollingArray@users.noreply.github.com>
2022-07-05 10:55:23 +0530

    Operating user model

--- app/src/app/shared/model/operating-user.model.ts
+++ app/src/app/shared/model/operating-user.model.ts
@@ -1,9 +1,19 @@
 /**
- * @author Ranjoy Sen
+ * © Rolling Array https://rollingarray.co.in/
+ *
+ *
+ * @summary Operating user model
+ * @author code@rollingarray.co.in
+ *
+ * Created at     : 2022-07-05 10:55:00 
+ * Last modified  : 2022-07-05 10:55:00 
  */
+
+
+import { OperationsEnum } from "../enum/operations.enum";
 
 
  export interface OperatingUserModel{
      userId ?: string;
-     operationType ?: string;
+     operationType ?: OperationsEnum;
  }
