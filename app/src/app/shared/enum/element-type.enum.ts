e7eaafe90a491a5c391b67ded5b554ab6b2373fb
Rolling Array <82917132+RollingArray@users.noreply.github.com>
2022-07-08 12:44:04 +0530

    Element type enum

--- app/src/app/shared/enum/element-type.enum.ts
+++ app/src/app/shared/enum/element-type.enum.ts
@@ -1,20 +1,29 @@
 /**
- * @author Ranjoy Sen
- * @email ranjoy.sen@rockwellcollins.com
- * @create date 2021-12-12 01:00:58
- * @modify date 2021-12-12 01:00:58
- * @desc Element type enum
+ * © Rolling Array https://rollingarray.co.in/
+ *
+ *
+ * @summary Element type enum
+ * @author code@rollingarray.co.in
+ *
+ * Created at     : 2022-06-30 11:38:29 
+ * Last modified  : 2022-07-08 12:43:56
  */
+
+
 
 export enum ElementTypeEnum{
     LEVEL_1 = "level-1",
     LEVEL_2 = "level-2",
+    INFO = "info",
+    IMAGE = "image",
+    VIDEO = "video",
+
     LEVEL_3 = "level-3",
     PARA = "para",
     PARA_IMAGE = "para-image",
     LIST_IMAGE = "list-image",
     LIST = "ol",
-    IMAGE = "image",
+    
     TEXT = "text",
     UNFORMATTED_TEXT = "unformatted-text",
     BOLD_TEXT = "bold-text",
