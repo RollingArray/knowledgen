815947b0cff48daa8bc48ecd7795342b323fdf02
Rolling Array <82917132+RollingArray@users.noreply.github.com>
2022-07-19 18:51:09 +0530

    Course material quiz answer model

---
+++ app/src/app/shared/model/course-material-quiz-answer.model.ts
@@ -0,0 +1,17 @@
+/**
+ * © Rolling Array https://rollingarray.co.in/
+ *
+ *
+ * @summary Course material quiz answer model
+ * @author code@rollingarray.co.in
+ *
+ * Created at     : 2022-07-13 13:53:27 
+ * Last modified  : 2022-07-13 15:02:56
+ */
+
+export interface CourseMaterialQuizAnswerModel
+{
+    answerId?: string;
+    answer?: string;
+    isCorrect?: boolean;
+}
