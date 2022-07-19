f451457a636220bb66d902fc3462ba57e4c290d1
Rolling Array <82917132+RollingArray@users.noreply.github.com>
2022-07-19 18:50:58 +0530

    Extend article model

--- app/src/app/shared/model/article-text-document.model.ts
+++ app/src/app/shared/model/article-text-document.model.ts
@@ -6,13 +6,14 @@
  * @author code@rollingarray.co.in
  *
  * Created at     : 2022-07-06 20:11:49 
- * Last modified  : 2022-07-06 20:48:38
+ * Last modified  : 2022-07-19 15:47:06
  */
 
 import { OperationsEnum } from "../enum/operations.enum";
+import { ArticleModel } from "./article.model";
 import { BaseModel } from "./base.model";
 
-export interface ArticleTextDocumentModel extends BaseModel
+export interface ArticleTextDocumentModel extends ArticleModel, BaseModel
 {
     articleId?: string;
     courseMaterialId?: string;
