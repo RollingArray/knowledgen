74cabc5f531841b7a9e4a8e2d4c695ed3c1130ae
Rolling Array <82917132+RollingArray@users.noreply.github.com>
2022-07-19 18:53:08 +0530

    Course material quiz state action

--- app/src/app/state/course-material-quiz/course-material-quiz.state.actions.ts
+++ app/src/app/state/course-material-quiz/course-material-quiz.state.actions.ts
@@ -6,7 +6,7 @@
  * @author code@rollingarray.co.in
  *
  * Created at     : 2022-07-13 20:07:37 
- * Last modified  : 2022-07-13 20:10:35
+ * Last modified  : 2022-07-19 18:43:49
  */
 
 import { createAction, props } from '@ngrx/store';
@@ -16,7 +16,7 @@ import { CourseMaterialQuizOperationsEnum } from './course-material-quiz-operati
 
 
 /**
- * @description Availability planner actions - Api Request Course Material Quiz
+ * @description Course material quiz actions - Api Request Course Material Quiz
  */
 export const API_REQUEST_COURSE_MATERIAL_QUIZ = createAction(
 	CourseMaterialQuizOperationsEnum.API_REQUEST_COURSE_MATERIAL_QUIZ,
@@ -24,7 +24,7 @@ export const API_REQUEST_COURSE_MATERIAL_QUIZ = createAction(
 );
 
 /**
- * @description Availability planner actions - Loaded Course Material Quiz
+ * @description Course material quiz actions - Loaded Course Material Quiz
  */
 export const LOADED_REQUEST_COURSE_MATERIAL_QUIZ = createAction(
 	CourseMaterialQuizOperationsEnum.LOADED_REQUEST_COURSE_MATERIAL_QUIZ,
@@ -32,7 +32,7 @@ export const LOADED_REQUEST_COURSE_MATERIAL_QUIZ = createAction(
 );
 
 /**
- * @description Availability planner actions - Act Upon Course Material Quiz
+ * @description Course material quiz actions - Act Upon Course Material Quiz
  */
 export const ACT_UPON_COURSE_MATERIAL_QUIZ = createAction(
 	CourseMaterialQuizOperationsEnum.ACT_UPON_COURSE_MATERIAL_QUIZ,
@@ -40,7 +40,7 @@ export const ACT_UPON_COURSE_MATERIAL_QUIZ = createAction(
 );
 
 /**
- * @description Availability planner actions - Send Api Request To Add New Course Material Quiz
+ * @description Course material quiz actions - Send Api Request To Add New Course Material Quiz
  */
 export const API_REQUEST_ADD_NEW_COURSE_MATERIAL_QUIZ = createAction(
 	CourseMaterialQuizOperationsEnum.API_REQUEST_ADD_NEW_COURSE_MATERIAL_QUIZ,
@@ -48,7 +48,7 @@ export const API_REQUEST_ADD_NEW_COURSE_MATERIAL_QUIZ = createAction(
 );
 
 /**
- * @description Availability planner actions - Send Api Request To Edit Course Material Quiz
+ * @description Course material quiz actions - Send Api Request To Edit Course Material Quiz
  */
 export const API_REQUEST_EDIT_COURSE_MATERIAL_QUIZ = createAction(
 	CourseMaterialQuizOperationsEnum.API_REQUEST_EDIT_COURSE_MATERIAL_QUIZ,
@@ -56,7 +56,7 @@ export const API_REQUEST_EDIT_COURSE_MATERIAL_QUIZ = createAction(
 );
 
 /**
- * @description Availability planner actions - Send Api Request To Delete Course Material Quiz
+ * @description Course material quiz actions - Send Api Request To Delete Course Material Quiz
  */
 export const API_REQUEST_DELETE_COURSE_MATERIAL_QUIZ = createAction(
 	CourseMaterialQuizOperationsEnum.API_REQUEST_DELETE_COURSE_MATERIAL_QUIZ,
@@ -64,7 +64,7 @@ export const API_REQUEST_DELETE_COURSE_MATERIAL_QUIZ = createAction(
 );
 
 /**
- * @description Availability planner actions - Store Newly Added Course Material Quiz
+ * @description Course material quiz actions - Store Newly Added Course Material Quiz
  */
 export const STORE_NEWLY_ADDED_COURSE_MATERIAL_QUIZ = createAction(
 	CourseMaterialQuizOperationsEnum.STORE_NEWLY_ADDED_COURSE_MATERIAL_QUIZ,
@@ -72,7 +72,7 @@ export const STORE_NEWLY_ADDED_COURSE_MATERIAL_QUIZ = createAction(
 );
 
 /**
- * @description Availability planner actions - Store Updated Course Material Quiz
+ * @description Course material quiz actions - Store Updated Course Material Quiz
  */
 export const STORE_UPDATED_COURSE_MATERIAL_QUIZ = createAction(
 	CourseMaterialQuizOperationsEnum.STORE_UPDATED_COURSE_MATERIAL_QUIZ,
@@ -80,7 +80,7 @@ export const STORE_UPDATED_COURSE_MATERIAL_QUIZ = createAction(
 );
 
 /**
- * @description Availability planner actions - Remove Course Material Quiz From Store
+ * @description Course material quiz actions - Remove Course Material Quiz From Store
  */
 export const REMOVE_COURSE_MATERIAL_QUIZ_FROM_STORE = createAction(
 	CourseMaterialQuizOperationsEnum.REMOVE_COURSE_MATERIAL_QUIZ_FROM_STORE,
@@ -88,42 +88,42 @@ export const REMOVE_COURSE_MATERIAL_QUIZ_FROM_STORE = createAction(
 );
 
 /**
- * @description Availability planner actions - Course Material Quiz CRUD Successfully
+ * @description Course material quiz actions - Course Material Quiz CRUD Successfully
  */
 export const COURSE_MATERIAL_QUIZ_CRUD_SUCCESS = createAction(
 	CourseMaterialQuizOperationsEnum.COURSE_MATERIAL_QUIZ_CRUD_SUCCESS
 );
 
 /**
- * @description Availability planner actions - Course Material Quiz CRUD Fail
+ * @description Course material quiz actions - Course Material Quiz CRUD Fail
  */
 export const COURSE_MATERIAL_QUIZ_CRUD_FAIL = createAction(
 	CourseMaterialQuizOperationsEnum.COURSE_MATERIAL_QUIZ_CRUD_FAIL
 );
 
 /**
- * @description Availability planner actions - Course Material Quiz Updated Successfully
+ * @description Course material quiz actions - Course Material Quiz Updated Successfully
  */
 export const COURSE_MATERIAL_QUIZ_UPDATED_SUCCESS = createAction(
 	CourseMaterialQuizOperationsEnum.COURSE_MATERIAL_QUIZ_UPDATED_SUCCESS
 );
 
 /**
- * @description Availability planner actions - Course Material Quiz Deleted Successfully
+ * @description Course material quiz actions - Course Material Quiz Deleted Successfully
  */
 export const COURSE_MATERIAL_QUIZ_DELETED_SUCCESS = createAction(
 	CourseMaterialQuizOperationsEnum.COURSE_MATERIAL_QUIZ_DELETED_SUCCESS
 );
 
 /**
- * @description Availability planner actions - Course Material Quiz Added Successfully
+ * @description Course material quiz actions - Course Material Quiz Added Successfully
  */
 export const COURSE_MATERIAL_QUIZ_ADDED_SUCCESS = createAction(
 	CourseMaterialQuizOperationsEnum.COURSE_MATERIAL_QUIZ_ADDED_SUCCESS
 );
 
 /**
- * @description Availability planner actions - No Operation
+ * @description Course material quiz actions - No Operation
  */
 export const NOOP = createAction(
 	CourseMaterialQuizOperationsEnum.NOOP,
