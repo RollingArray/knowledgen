36179c2dc4b01a0ef9b928ceb0aa1e9d2616eb5d
Rolling Array <82917132+RollingArray@users.noreply.github.com>
2022-07-19 09:54:59 +0530

    Article status type enum

---
+++ app/src/app/shared/enum/article-status-type.enum.ts
@@ -0,0 +1,17 @@
+/**
+ * © Rolling Array https://rollingarray.co.in/
+ *
+ *
+ * @summary Article status type enum
+ * @author code@rollingarray.co.in
+ *
+ * Created at     : 2022-07-18 19:12:38 
+ * Last modified  : 2022-07-18 19:12:56
+ */
+
+
+
+export enum ArticleStatusTypeEnum{
+	PREVIEW = 'P',
+	LIVE = 'L',
+}
