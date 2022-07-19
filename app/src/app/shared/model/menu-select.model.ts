1f3c9800420817390e76f7729bf792b5e4717c16
Rolling Array <82917132+RollingArray@users.noreply.github.com>
2022-07-19 09:58:27 +0530

    Extend ArticleModel

--- app/src/app/shared/model/menu-select.model.ts
+++ app/src/app/shared/model/menu-select.model.ts
@@ -6,16 +6,15 @@
  * @author code@rollingarray.co.in
  *
  * Created at     : 2022-07-05 19:37:36 
- * Last modified  : 2022-07-05 19:38:07
+ * Last modified  : 2022-07-18 19:44:12
  */
 
 
 import { CourseMaterialTypeIdEnum } from '../enum/course-material-type-id.enum';
 import { MenuTypeEnum } from '../enum/menu-type.enum';
+import { ArticleModel } from './article.model';
 
-export interface MenuSelectModel {
-	articleId: string,
-	courseMaterialId: string,
+export interface MenuSelectModel extends ArticleModel {
 	menuType: MenuTypeEnum,
 	courseMaterialType: CourseMaterialTypeIdEnum
 }
