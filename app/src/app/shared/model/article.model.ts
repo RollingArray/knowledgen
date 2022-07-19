6ef7f9842186fa971b9e2b0961ebe86742cbf8a8
Rolling Array <82917132+RollingArray@users.noreply.github.com>
2022-07-19 09:56:34 +0530

    Add articleStatus to article model

--- app/src/app/shared/model/article.model.ts
+++ app/src/app/shared/model/article.model.ts
@@ -2,19 +2,20 @@
  * © Rolling Array https://rollingarray.co.in/
  *
  *
- * @summary Modal data
+ * @summary Article model
  * @author code@rollingarray.co.in
  *
  * Created at     : 2021-05-18 19:11:57 
- * Last modified  : 2022-01-20 01:53:16
+ * Last modified  : 2022-07-18 19:37:51
  */
 
+import { ArticleStatusTypeEnum } from "../enum/article-status-type.enum";
 import { BaseModel } from "./base.model";
-
 
 export interface ArticleModel extends BaseModel
 {
 	courseMaterialId?: string;
 	articleId?: string;
 	articleTitle?: string;
+	articleStatus?: ArticleStatusTypeEnum;
 }
