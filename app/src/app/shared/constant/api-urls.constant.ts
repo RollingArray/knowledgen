8fb207c8424ed1338425bc733615488019672d8a
Rolling Array <82917132+RollingArray@users.noreply.github.com>
2022-07-19 09:54:30 +0530

    Add quiz related urls

--- app/src/app/shared/constant/api-urls.constant.ts
+++ app/src/app/shared/constant/api-urls.constant.ts
@@ -7,7 +7,7 @@
  * @author code@rollingarray.co.in
  *
  * Created at     : 2021-11-15 21:34:14 
- * Last modified  : 2022-07-12 11:32:34
+ * Last modified  : 2022-07-18 21:22:34
  */
 
 import { environment } from "../../../environments/environment";
@@ -27,40 +27,53 @@ export class ApiUrls {
 	public static readonly USER_DETAILS: string = ApiUrls.API_BASE_PATH + "/user/details";
 	public static readonly USER_PROFILE_UPDATE: string = ApiUrls.API_BASE_PATH + "/user/edit";
 
-	public static readonly COURSE_MATERIAL: string = ApiUrls.API_BASE_PATH + "/course/material/all";
-	public static readonly RECOMMENDED_COURSE_MATERIAL: string = ApiUrls.API_BASE_PATH + "/course/material/recommended/all";
-	public static readonly COURSE_MATERIAL_ADD: string = ApiUrls.API_BASE_PATH + "/course/material/add";
-	public static readonly COURSE_MATERIAL_EDIT: string = ApiUrls.API_BASE_PATH + "/course/material/edit";
-	public static readonly COURSE_MATERIAL_DELETE: string = ApiUrls.API_BASE_PATH + "/course/material/delete";
+	public static readonly COURSE_MATERIAL_BASE: string = ApiUrls.API_BASE_PATH + "/course/material";
+	public static readonly COURSE_MATERIAL: string = ApiUrls.COURSE_MATERIAL_BASE + "/all";
+	public static readonly RECOMMENDED_COURSE_MATERIAL: string = ApiUrls.COURSE_MATERIAL_BASE + "/recommended/all";
+	public static readonly COURSE_MATERIAL_ADD: string = ApiUrls.COURSE_MATERIAL_BASE + "/add";
+	public static readonly COURSE_MATERIAL_EDIT: string = ApiUrls.COURSE_MATERIAL_BASE + "/edit";
+	public static readonly COURSE_MATERIAL_DELETE: string = ApiUrls.COURSE_MATERIAL_BASE + "/delete";
 
-	public static readonly COURSE_MATERIAL_MENU: string = ApiUrls.API_BASE_PATH + "/course/material/menu/all";
+	public static readonly COURSE_MATERIAL_MENU_BASE: string = ApiUrls.COURSE_MATERIAL_BASE + "/menu";
+	public static readonly COURSE_MATERIAL_MENU: string = ApiUrls.COURSE_MATERIAL_MENU_BASE + "/all";
 
-	public static readonly COURSE_MATERIAL_PARENT_MENU_ADD: string = ApiUrls.API_BASE_PATH + "/course/material/menu/add";
-	public static readonly COURSE_MATERIAL_PARENT_MENU_EDIT: string = ApiUrls.API_BASE_PATH + "/course/material/menu/edit";
-	public static readonly COURSE_MATERIAL_PARENT_MENU_DELETE: string = ApiUrls.API_BASE_PATH + "/course/material/menu/delete";
+	public static readonly COURSE_MATERIAL_PARENT_MENU_ADD: string = ApiUrls.COURSE_MATERIAL_MENU_BASE + "/add";
+	public static readonly COURSE_MATERIAL_PARENT_MENU_EDIT: string = ApiUrls.COURSE_MATERIAL_MENU_BASE + "/edit";
+	public static readonly COURSE_MATERIAL_PARENT_MENU_DELETE: string = ApiUrls.COURSE_MATERIAL_MENU_BASE + "/delete";
 
-	public static readonly COURSE_MATERIAL_CHILD_MENU_ADD: string = ApiUrls.API_BASE_PATH + "/course/material/menu/child/add";
-	public static readonly COURSE_MATERIAL_CHILD_MENU_EDIT: string = ApiUrls.API_BASE_PATH + "/course/material/menu/child/edit";
-	public static readonly COURSE_MATERIAL_CHILD_MENU_DELETE: string = ApiUrls.API_BASE_PATH + "/course/material/menu/child/delete";
+	public static readonly COURSE_MATERIAL_CHILD_MENU_BASE: string = ApiUrls.COURSE_MATERIAL_MENU_BASE + "/child";
+	public static readonly COURSE_MATERIAL_CHILD_MENU_ADD: string = ApiUrls.COURSE_MATERIAL_CHILD_MENU_BASE + "/add";
+	public static readonly COURSE_MATERIAL_CHILD_MENU_EDIT: string = ApiUrls.COURSE_MATERIAL_CHILD_MENU_BASE + "/edit";
+	public static readonly COURSE_MATERIAL_CHILD_MENU_DELETE: string = ApiUrls.COURSE_MATERIAL_CHILD_MENU_BASE + "/delete";
 
-	public static readonly COURSE_MATERIAL_SUB_CHILD_MENU_ADD: string = ApiUrls.API_BASE_PATH + "/course/material/menu/child/sub/add";
-	public static readonly COURSE_MATERIAL_SUB_CHILD_MENU_EDIT: string = ApiUrls.API_BASE_PATH + "/course/material/menu/child/sub/edit";
-	public static readonly COURSE_MATERIAL_SUB_CHILD_MENU_DELETE: string = ApiUrls.API_BASE_PATH + "/course/material/menu/child/sub/delete";
+	public static readonly COURSE_MATERIAL_SUB_CHILD_MENU_BASE: string = ApiUrls.COURSE_MATERIAL_CHILD_MENU_BASE + "/sub";
+	public static readonly COURSE_MATERIAL_SUB_CHILD_MENU_ADD: string = ApiUrls.COURSE_MATERIAL_SUB_CHILD_MENU_BASE + "/add";
+	public static readonly COURSE_MATERIAL_SUB_CHILD_MENU_EDIT: string = ApiUrls.COURSE_MATERIAL_SUB_CHILD_MENU_BASE + "/edit";
+	public static readonly COURSE_MATERIAL_SUB_CHILD_MENU_DELETE: string = ApiUrls.COURSE_MATERIAL_SUB_CHILD_MENU_BASE + "/delete";
 
-	public static readonly TEACHER_AVAILABILITY_PLANNER: string = ApiUrls.API_BASE_PATH + "/teacher/availability/planner/all";
-	public static readonly STUDENT_AVAILABILITY_PLANNER: string = ApiUrls.API_BASE_PATH + "/student/availability/planner/all";
+	public static readonly TEACHER_AVAILABILITY_PLANNER_BASE: string = ApiUrls.API_BASE_PATH + "/teacher/availability/planner";
+	public static readonly TEACHER_AVAILABILITY_PLANNER: string = ApiUrls.TEACHER_AVAILABILITY_PLANNER_BASE + "/all";
+	public static readonly TEACHER_AVAILABILITY_PLANNER_ADD: string = ApiUrls.TEACHER_AVAILABILITY_PLANNER + "/add";
+	public static readonly TEACHER_AVAILABILITY_PLANNER_EDIT: string = ApiUrls.TEACHER_AVAILABILITY_PLANNER + "/edit";
+	public static readonly TEACHER_AVAILABILITY_PLANNER_DELETE: string = ApiUrls.TEACHER_AVAILABILITY_PLANNER + "/delete";
+
+	public static readonly STUDENT_AVAILABILITY_PLANNER_BASE: string = ApiUrls.API_BASE_PATH + "/student/availability/planner";
+	public static readonly STUDENT_AVAILABILITY_PLANNER: string = ApiUrls.STUDENT_AVAILABILITY_PLANNER_BASE + "/all";
+	public static readonly STUDENT_AVAILABILITY_PLANNER_ADD: string = ApiUrls.STUDENT_AVAILABILITY_PLANNER_BASE + "/add";
+	public static readonly STUDENT_AVAILABILITY_PLANNER_EDIT: string = ApiUrls.STUDENT_AVAILABILITY_PLANNER_BASE + "/edit";
+	public static readonly STUDENT_AVAILABILITY_PLANNER_DELETE: string = ApiUrls.STUDENT_AVAILABILITY_PLANNER_BASE + "/delete";
+
+	public static readonly COURSE_MATERIAL_ARTICLE_BASE: string = ApiUrls.API_BASE_PATH + "/course/material/article";
+	public static readonly COURSE_MATERIAL_TEXT_DOC: string = ApiUrls.COURSE_MATERIAL_ARTICLE_BASE + "/textdocument";
+	public static readonly KNOWLEDGE_BASE_ARTICLE_UPLOAD_FILE: string = ApiUrls.COURSE_MATERIAL_ARTICLE_BASE + "/upload/file";
+	public static readonly KNOWLEDGE_BASE_ARTICLE_TEXT_DOCUMENT: string = ApiUrls.COURSE_MATERIAL_TEXT_DOC + "/all";
+	public static readonly KNOWLEDGE_BASE_ARTICLE_TEXT_DOCUMENT_ADD: string = ApiUrls.COURSE_MATERIAL_TEXT_DOC + "/add";
+	public static readonly KNOWLEDGE_BASE_ARTICLE_TEXT_DOCUMENT_EDIT: string = ApiUrls.COURSE_MATERIAL_TEXT_DOC + "/edit";
 	
-	public static readonly TEACHER_AVAILABILITY_PLANNER_ADD: string = ApiUrls.API_BASE_PATH + "/teacher/availability/planner/add";
-	public static readonly TEACHER_AVAILABILITY_PLANNER_EDIT: string = ApiUrls.API_BASE_PATH + "/teacher/availability/planner/edit";
-	public static readonly TEACHER_AVAILABILITY_PLANNER_DELETE: string = ApiUrls.API_BASE_PATH + "/teacher/availability/planner/delete";
+	public static readonly COURSE_MATERIAL_QUIZ_BASE: string = ApiUrls.COURSE_MATERIAL_ARTICLE_BASE + "/quiz";
+	public static readonly COURSE_MATERIAL_QUIZ: string = ApiUrls.COURSE_MATERIAL_QUIZ_BASE + "/all";
+	public static readonly COURSE_MATERIAL_QUIZ_ADD: string = ApiUrls.COURSE_MATERIAL_QUIZ_BASE + "/add";
+	public static readonly COURSE_MATERIAL_QUIZ_EDIT: string = ApiUrls.COURSE_MATERIAL_QUIZ_BASE + "/edit";
+	public static readonly COURSE_MATERIAL_QUIZ_DELETE: string = ApiUrls.COURSE_MATERIAL_QUIZ_BASE + "/delete";
 
-	public static readonly STUDENT_AVAILABILITY_PLANNER_ADD: string = ApiUrls.API_BASE_PATH + "/student/availability/planner/add";
-	public static readonly STUDENT_AVAILABILITY_PLANNER_EDIT: string = ApiUrls.API_BASE_PATH + "/student/availability/planner/edit";
-	public static readonly STUDENT_AVAILABILITY_PLANNER_DELETE: string = ApiUrls.API_BASE_PATH + "/student/availability/planner/delete";
-
-	public static readonly KNOWLEDGE_BASE_ARTICLE_TEXT_DOCUMENT: string = ApiUrls.API_BASE_PATH + "/course/material/article/textdocument/all";
-	public static readonly KNOWLEDGE_BASE_ARTICLE_TEXT_DOCUMENT_ADD: string = ApiUrls.API_BASE_PATH + "/course/material/article/textdocument/add";
-	public static readonly KNOWLEDGE_BASE_ARTICLE_TEXT_DOCUMENT_EDIT: string = ApiUrls.API_BASE_PATH + "/course/material/article/textdocument/edit";
-	public static readonly KNOWLEDGE_BASE_ARTICLE_UPLOAD_FILE: string = ApiUrls.API_BASE_PATH + "/course/material/article/upload/file";
-	
 }
