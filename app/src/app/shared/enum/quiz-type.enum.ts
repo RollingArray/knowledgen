2dcab52fe3d12edb74e0aea41870b36000255a23
Rolling Array <82917132+RollingArray@users.noreply.github.com>
2022-07-19 09:55:46 +0530

    Quiz type enum

---
+++ app/src/app/shared/enum/quiz-type.enum.ts
@@ -0,0 +1,16 @@
+/**
+ * © Rolling Array https://rollingarray.co.in/
+ *
+ *
+ * @summary Quiz type enum
+ * @author code@rollingarray.co.in
+ *
+ * Created at     : 2022-07-13 13:46:47 
+ * Last modified  : 2022-07-13 13:51:07
+ */
+
+export enum QuizTypeEnum{
+	MCQ = 'mcq',
+	TF = 'trueFalse',
+	OE = 'openEnded'
+}
