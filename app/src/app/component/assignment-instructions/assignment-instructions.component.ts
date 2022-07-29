7f6a5c15f07e2b1c02ac373e73830fd7afcdfb55
Rolling Array <82917132+RollingArray@users.noreply.github.com>
2022-07-29 16:30:49 +0530

    Remove un wanted imports

--- app/src/app/component/assignment-instructions/assignment-instructions.component.ts
+++ app/src/app/component/assignment-instructions/assignment-instructions.component.ts
@@ -6,38 +6,19 @@
  * @author code@rollingarray.co.in
  *
  * Created at     : 2022-07-27 18:49:32 
- * Last modified  : 2022-07-27 18:50:09
+ * Last modified  : 2022-07-28 09:04:18
  */
 
-import { Component, OnInit, ViewChild, ElementRef, Injector, Inject, Input, EventEmitter, Output } from "@angular/core";
-import { TranslateService } from "@ngx-translate/core";
-import { CookieService } from "ngx-cookie-service";
+import { Component, OnInit, Injector, Input, EventEmitter, Output } from "@angular/core";
 import { Observable } from "rxjs";
 import { takeUntil } from "rxjs/operators";
 import { ArrayKey } from "src/app/shared/constant/array.constant";
-import { LocalStoreKey } from "src/app/shared/constant/local-store-key.constant";
 import { StringKey } from "src/app/shared/constant/string.constant";
 import { CourseMaterialTypeIdEnum } from "src/app/shared/enum/course-material-type-id.enum";
 import { OperationsEnum } from "src/app/shared/enum/operations.enum";
-import { QuizTypeEnum } from "src/app/shared/enum/quiz-type.enum";
-import { AssignmentTimeModel } from "src/app/shared/model/assignment-time.model";
-import { CourseMaterialAssignmentResultModel } from "src/app/shared/model/course-material-assignment-result.model";
-import { CourseMaterialQuizAnswerModel } from "src/app/shared/model/course-material-quiz-answer.model";
-import { CourseMaterialQuizModel } from "src/app/shared/model/course-material-quiz.model";
-import { CourseMaterialModel } from "src/app/shared/model/course-material.model";
 import { MenuSelectModel } from "src/app/shared/model/menu-select.model";
-import { ModalData } from "src/app/shared/model/modal-data.model";
-import { AlertService } from "src/app/shared/service/alert.service";
-import { ToastService } from "src/app/shared/service/toast.service";
-import { UtilityService } from "src/app/shared/service/utility.service";
-import { CourseMaterialAssignmentStateFacade } from "src/app/state/course-material-assignment/course-material-assignment.state.facade";
 import { CourseMaterialMenuStateFacade } from "src/app/state/course-material-menu/course-material-menu.state.facade";
-import { CourseMaterialQuizStateFacade } from "src/app/state/course-material-quiz/course-material-quiz.state.facade";
-import { CourseMaterialStateFacade } from "src/app/state/course-material/course-material.state.facade";
-import { RootStateFacade } from "src/app/state/root/root.state.facade";
 import { BaseFormComponent } from "../base/base-form.component";
-import { CrudCourseMaterialAssignmentResultComponent } from "../crud-course-material-assignment-result/crud-course-material-assignment-result.component";
-import { CrudQuizQuestionComponent } from "../crud-quiz-question/crud-quiz-question.component";
 
 @Component({
 	selector: 'assignment-instructions',
