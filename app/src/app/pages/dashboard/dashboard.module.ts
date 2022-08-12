41b2e24884e35ef5a33c3456bdf4a89730d8cbbc
Rolling Array <82917132+RollingArray@users.noreply.github.com>
2022-08-12 20:08:34 +0530

    Dashboard page

---
+++ app/src/app/pages/dashboard/dashboard.module.ts
@@ -0,0 +1,60 @@
+/**
+ * © Rolling Array https://rollingarray.co.in/
+ *
+ *
+ * @summary Dashboard page module
+ * @author code@rollingarray.co.in
+ *
+ * Created at     : 2022-08-12 20:05:53 
+ * Last modified  : 2022-08-12 20:06:09
+ */
+
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { IonicModule } from "@ionic/angular";
+import { AnalysisBoothModule } from "src/app/component/analysis-booth/analysis-booth.module";
+import { AssignmentsScoreAnalysisModule } from "src/app/component/assignments-score-analysis/assignments-score-analysis.module";
+import { CourseContentCoverageModule } from "src/app/component/course-content-coverage/course-content-coverage.module";
+import { CourseContentTimeCoverageModule } from "src/app/component/course-content-time-coverage/course-content-time-coverage.module";
+import { NoDataModule } from "src/app/component/no-data/no-data.component.module";
+import { PageInfoTitleModule } from "src/app/component/page-info-title/page-info-title.component.module";
+import { PanelHeaderModule } from "src/app/component/panel-header/panel-header.component.module";
+import { PanelInfoModule } from "src/app/component/panel-info/panel-info.component.module";
+import { I18nModule } from "src/app/shared/module/i18n.module";
+import { SharedModule } from "src/app/shared/module/shared.module";
+import { DashboardStateModule } from "src/app/state/dashboard/dashboard.state.module";
+import { RootStateModule } from "src/app/state/root/root.state.module";
+import { DashboardPage } from "./dashboard.page";
+
+const routes: Routes = [
+	{
+		path: '',
+		component: DashboardPage
+	},
+];
+
+@NgModule({
+	imports: [
+		CommonModule,
+		IonicModule,
+		SharedModule,
+		NoDataModule,
+		PageInfoTitleModule,
+		PanelHeaderModule,
+		PanelInfoModule,
+		I18nModule,
+		RootStateModule,
+		DashboardStateModule,
+		CourseContentCoverageModule,
+		CourseContentTimeCoverageModule,
+		AssignmentsScoreAnalysisModule,
+		AnalysisBoothModule,
+		RouterModule.forChild(routes)
+	],
+	declarations: [DashboardPage],
+	providers: [
+
+	]
+})
+export class DashboardPageModule { }
---
+++ app/src/app/pages/dashboard/dashboard.page.ts
@@ -0,0 +1,238 @@
+/**
+ * © Rolling Array https://rollingarray.co.in/
+ *
+ *
+ * @summary Dashboard page
+ * @author code@rollingarray.co.in
+ *
+ * Created at     : 2022-08-12 20:05:53 
+ * Last modified  : 2022-08-12 20:08:19
+ */
+
+import { Component, OnInit, OnDestroy, Injector } from "@angular/core";
+import { TranslateService } from "@ngx-translate/core";
+import { CookieService } from "ngx-cookie-service";
+import { Observable } from "rxjs";
+import { takeUntil } from "rxjs/operators";
+import { BaseViewComponent } from "src/app/component/base/base-view.component";
+import { LocalStoreKey } from "src/app/shared/constant/local-store-key.constant";
+import { CharacteristicsEnum } from "src/app/shared/enum/characteristics.enum";
+import { OperationsEnum } from "src/app/shared/enum/operations.enum";
+import { UserTypeEnum } from "src/app/shared/enum/user-type.enum";
+import { DashboardStudentModel } from "src/app/shared/model/dashboard-student.model";
+import { LocalStorageService } from "src/app/shared/service/local-storage.service";
+import { DashboardStateFacade } from "src/app/state/dashboard/dashboard.state.facade";
+import { RootStateFacade } from "src/app/state/root/root.state.facade";
+
+@Component({
+	selector: "dashboard",
+	templateUrl: "./dashboard.page.html",
+	styleUrls: ["./dashboard.page.scss"]
+})
+export class DashboardPage extends BaseViewComponent implements OnInit, OnDestroy
+{
+
+	/**
+	 * -------------------------------------------------|
+	 * @description										|
+	 * @readonly properties								|
+	 * -------------------------------------------------|
+	 */
+	readonly operationsEnum = OperationsEnum;
+
+	/**
+	 * Characteristics enum of dashboard page
+	 */
+	readonly characteristicsEnum = CharacteristicsEnum;
+
+	/**
+	 * User type enum of dashboard page
+	 */
+	readonly userTypeEnum = UserTypeEnum;
+
+	/**
+	 * -------------------------------------------------|
+	 * @description										|
+	 * @private Instance variable								|
+	 * -------------------------------------------------|
+	 */
+
+	/**
+	 * -------------------------------------------------|
+	 * @description										|
+	 * @public Instance variable								|
+	 * -------------------------------------------------|
+	 */
+	/**
+	 * Description  of dashboard page
+	 */
+	dashboardStudent$!: Observable<DashboardStudentModel>;
+
+	/**
+	 * Determines whether student data$ has
+	 */
+	hasStudentData$!: Observable<boolean>;
+
+	/**
+	 * -------------------------------------------------|
+	 * @description										|
+	 * Getter & Setters									|
+	 * -------------------------------------------------|
+	 */
+
+	/**
+	 * Gets description
+	 */
+	get userType()
+	{
+		return this.cookieService.get(LocalStoreKey.LOGGED_IN_USER_TYPE);
+	}
+
+	/**
+	 * Gets whether is user type teacher
+	 */
+	get isUserTypeTeacher()
+	{
+		return this.userType === UserTypeEnum.Teacher ? true : false;
+	}
+
+	/**
+	 * Gets whether is user type student
+	 */
+	get isUserTypeStudent()
+	{
+		return this.userType === UserTypeEnum.Student ? true : false;
+	}
+
+	/**
+	 * Gets logged in user
+	 */
+	public get loggedInUser(): string
+	{
+		let loggedInUserName = '';
+		this.localStorageService
+			.getActiveUserName()
+			.pipe(takeUntil(this.unsubscribe))
+			.subscribe((data: string) =>
+			{
+				loggedInUserName = data;
+			});
+
+		return loggedInUserName;
+	}
+
+	/**
+	 * Gets greeting
+	 */
+	get greeting()
+	{
+		const hr = new Date().getHours();
+		let greet = '';
+
+		if (hr < 12)
+		{
+			greet = 'pageTitle.morning';
+		}
+		else if (hr >= 12 && hr <= 17)
+		{
+			greet = 'pageTitle.afternoon';
+		}
+		else if (hr >= 17 && hr <= 24)
+		{
+			greet = 'pageTitle.evening';
+		}
+
+		return greet;
+	}
+
+	/**
+	 * -------------------------------------------------|
+	 * @description										|
+	 * Life cycle hook									|
+	 * -------------------------------------------------|
+	 */
+	/**
+	 * Creates an instance of dashboard page.
+	 * @param injector 
+	 * @param dashboardStateFacade 
+	 * @param rootStateFacade 
+	 * @param translateService 
+	 * @param cookieService 
+	 * @param localStorageService 
+	 */
+	constructor(
+		injector: Injector,
+		private dashboardStateFacade: DashboardStateFacade,
+		private rootStateFacade: RootStateFacade,
+		private translateService: TranslateService,
+		private cookieService: CookieService,
+		private localStorageService: LocalStorageService,
+	)
+	{
+		super(injector);
+	}
+
+	/**
+	 * on init
+	 */
+	async ngOnInit()
+	{
+		this.translateService
+			.get('loading.holdTight')
+			.pipe(takeUntil(this.unsubscribe))
+			.subscribe(async (data: string) =>
+			{
+				this.errorMessage = data;
+			});
+		this.loadData();
+	}
+
+	/**
+	 * -------------------------------------------------|
+	 * @description										|
+	 * @Public methods									|
+	 * -------------------------------------------------|
+	 */
+
+	/**
+	 * @description Loads data
+	 */
+	public loadData()
+	{
+		if (this.userType === UserTypeEnum.Student)
+		{
+			this.hasStudentData$ = this.dashboardStateFacade.hasStudentData$;
+			this.dashboardStudent$ = this.dashboardStateFacade.dashboardStudent$;
+
+			// if no data available ... make a api request, else work with store data
+			this.hasStudentData$
+				.pipe(takeUntil(this.unsubscribe))
+				.subscribe(
+					hasData =>
+					{
+						if (!hasData)
+						{
+							this.getDashboard()
+						}
+					}
+				);
+		}
+
+	}
+
+	/**
+	 * Gets course material material
+	 */
+	async getDashboard()
+	{
+		this.translateService
+			.get('loading.wait')
+			.pipe(takeUntil(this.unsubscribe))
+			.subscribe(async (data: string) =>
+			{
+				await this.rootStateFacade.startLoading(data);
+			});
+
+		this.dashboardStateFacade.requestDashboardStudent();
+	}
+}
