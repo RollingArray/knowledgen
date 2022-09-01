2fd659ed904136c77d17c39c89fc2752ef2a9164
Rolling Array <82917132+RollingArray@users.noreply.github.com>
2022-09-01 18:22:28 +0530

    Result type enum

---
+++ app/src/app/shared/enum/retust-type.enum.ts
@@ -0,0 +1,15 @@
+/**
+ * © Rolling Array https://rollingarray.co.in/
+ *
+ *
+ * @summary Result type enum
+ * @author code@rollingarray.co.in
+ *
+ * Created at     : 2022-08-29 19:57:12 
+ * Last modified  : 2022-08-29 19:57:30
+ */
+
+export enum ResultTypeEnum{
+	SCORE = 'score',
+	TIME = 'time',
+}
