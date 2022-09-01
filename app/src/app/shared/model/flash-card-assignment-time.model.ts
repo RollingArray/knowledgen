c586b79f11924b8c0fbf11c5a89f9298856e52fc
Rolling Array <82917132+RollingArray@users.noreply.github.com>
2022-09-01 18:23:04 +0530

    Flash card assignment time model

---
+++ app/src/app/shared/model/flash-card-assignment-time.model.ts
@@ -0,0 +1,20 @@
+/**
+ * © Rolling Array https://rollingarray.co.in/
+ *
+ *
+ * @summary Flash card assignment time model
+ * @author code@rollingarray.co.in
+ *
+ * Created at     : 2022-08-26 09:24:37 
+ * Last modified  : 2022-08-26 10:00:48
+ */
+
+export interface FlashCardAssignmentTimeModel
+{
+    cardId: string,
+    startTime?: number,
+    endTime?: number,
+    spendTime?: number
+}
+
+
