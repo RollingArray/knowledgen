55b3a34969a6a7c67bd14580efcc7af055724c34
Rolling Array <82917132+RollingArray@users.noreply.github.com>
2022-09-01 18:22:06 +0530

    Media type enum

---
+++ app/src/app/shared/enum/media-type.enum.ts
@@ -0,0 +1,19 @@
+/**
+ * © Rolling Array https://rollingarray.co.in/
+ *
+ *
+ * @summary Media type enum
+ * @author code@rollingarray.co.in
+ *
+ * Created at     : 2022-08-18 08:06:23 
+ * Last modified  : 2022-08-18 08:08:28
+ */
+
+
+
+export enum MediaTypeEnum{
+	VIDEO = 'video',
+	IMAGE = 'image',
+	AUDIO = 'audio',
+	NONE = 'none'
+}
