79a0bbcaaa4daf9e3a160bd4b1e6ee8d21a3c32e
Rolling Array <82917132+RollingArray@users.noreply.github.com>
2022-09-20 11:22:28 +0530

    APP: Modify user avatar to accept user initials

--- app/src/app/component/avatar/avatar.component.module.ts
+++ app/src/app/component/avatar/avatar.component.module.ts
@@ -1,3 +1,13 @@
+/**
+ * © Rolling Array https://rollingarray.co.in/
+ *
+ * @summary Avatar component module
+ * @author code@rollingarray.co.in
+ *
+ * Created at     : 2022-09-20 11:18:54 
+ * Last modified  : 2022-09-20 11:19:32
+ */	
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
--- app/src/app/component/avatar/avatar.component.ts
+++ app/src/app/component/avatar/avatar.component.ts
@@ -1,22 +1,140 @@
+/**
+ * © Rolling Array https://rollingarray.co.in/
+ *
+ * @summary Avatar component
+ * @author code@rollingarray.co.in
+ *
+ * Created at     : 2022-09-20 11:18:54 
+ * Last modified  : 2022-09-20 11:21:50
+ */
+
 import { BaseViewComponent } from 'src/app/component/base/base-view.component';
-import { Component, OnInit, Input, Output, EventEmitter, Injector } from '@angular/core';
+import
+	{
+		Component,
+		OnInit,
+		Input,
+		Output,
+		EventEmitter,
+		Injector,
+	} from '@angular/core';
 
 @Component({
-  selector: 'app-avatar',
-  templateUrl: './avatar.component.html',
-  styleUrls: ['./avatar.component.scss'],
+	selector: 'avatar',
+	templateUrl: './avatar.component.html',
+	styleUrls: ['./avatar.component.scss'],
 })
-export class AvatarComponent extends BaseViewComponent implements OnInit {
+export class AvatarComponent extends BaseViewComponent implements OnInit
+{
+	/**
+	 * -------------------------------------------------|
+	 * @description                                     |
+	 * @readonly properties                             |
+	 * -------------------------------------------------|
+	 */
 
-  @Input() user;
-  @Input() icon;
-  
-  constructor(
-    injector: Injector,
-  ) { 
-    super(injector);
-  }
+	/**
+	 * -------------------------------------------------|
+	 * @description                                     |
+	 * @private Instance variable                       |
+	 * -------------------------------------------------|
+	 */
 
-  ngOnInit() {}
+	/**
+	 * -------------------------------------------------|
+	 * @description                                     |
+	 * @public Instance variable                        |
+	 * -------------------------------------------------|
+	 */
 
+	/**
+	 * -------------------------------------------------|
+	 * @description                                     |
+	 * Getter & Setters                                 |
+	 * -------------------------------------------------|
+	 */
+
+	/**
+	 * Gets description
+	 */
+	get userNameInitials()
+	{
+		return (
+			this.firstName[0].toUpperCase() +
+			'' +
+			this.lastName[0].toUpperCase()
+		);
+	}
+
+	/**
+	 * -------------------------------------------------|
+	 * @description                                     |
+	 * @ViewChild Instance variable                     |
+	 * -------------------------------------------------|
+	 */
+
+	/**
+	 * -------------------------------------------------|
+	 * @description										|
+	 * @input & @output Instance variable				|
+	 * -------------------------------------------------|
+	 */
+
+	/**
+	 * Description  of avatar component
+	 */
+	@Input() firstName = '';
+
+	/**
+	 * Input  of avatar component
+	 */
+	@Input() lastName = '';
+
+	/**
+	 * Input  of avatar component
+	 */
+	@Input() profileImage = '';
+
+	/**
+	 * Input  of avatar component
+	 */
+	@Input() profileSize = 'normal';
+
+	/**
+	 * -------------------------------------------------|
+	 * @description                                     |
+	 * Life cycle hook                                  |
+	 * -------------------------------------------------|
+	 */
+
+	/**
+	 * Creates an instance of avatar component.
+	 * @param injector 
+	 */
+	constructor(injector: Injector)
+	{
+		super(injector);
+	}
+
+	/**
+	 * on init
+	 */
+	ngOnInit()
+	{
+		//
+	}
+
+	/**
+	 * -------------------------------------------------|
+	 * @description                                     |
+	 * @Private methods                                 |
+	 * -------------------------------------------------|
+	 */
+
+	/**
+	 * -------------------------------------------------|
+	 * @description                                     |
+	 * @Public methods                                  |
+	 * -------------------------------------------------|
+	 */
 }
