b3878d9c0eea33a9bcc1ad06bb8ff85d5a3f126a
Rolling Array <82917132+RollingArray@users.noreply.github.com>
2022-09-20 11:25:51 +0530

    APP: Get user updated data via state

--- app/src/app/component/user-profile/user-profile.component.module.ts
+++ app/src/app/component/user-profile/user-profile.component.module.ts
@@ -1,14 +1,36 @@
+/**
+ * © Rolling Array https://rollingarray.co.in/
+ *
+ * @summary User profile component module
+ * @author code@rollingarray.co.in
+ *
+ * Created at     : 2022-09-20 08:43:51 
+ * Last modified  : 2022-09-20 08:44:49
+ */
+
 import { PageInfoTitleModule } from './../page-info-title/page-info-title.component.module';
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { SharedModule } from "src/app/shared/module/shared.module";
-import { IonicModule } from "@ionic/angular";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from 'src/app/shared/module/shared.module';
+import { IonicModule } from '@ionic/angular';
 import { UserProfileComponent } from './user-profile.component';
 import { I18nModule } from 'src/app/shared/module/i18n.module';
+import { RootStateModule } from 'src/app/state/root/root.state.module';
+import { ContentLoadingModule } from '../content-loading/content-loading.module';
+import { AvatarModule } from '../avatar/avatar.component.module';
 @NgModule({
-  imports: [CommonModule, SharedModule, IonicModule, PageInfoTitleModule, I18nModule],
-  declarations: [UserProfileComponent],
-  exports: [UserProfileComponent],
-  entryComponents: [UserProfileComponent]
+	imports: [
+		CommonModule,
+		SharedModule,
+		IonicModule,
+		PageInfoTitleModule,
+		I18nModule,
+		RootStateModule,
+		ContentLoadingModule,
+		AvatarModule
+	],
+	declarations: [UserProfileComponent],
+	exports: [UserProfileComponent],
+	entryComponents: [UserProfileComponent],
 })
-export class UserProfileModule {}
+export class UserProfileModule { }
--- app/src/app/component/user-profile/user-profile.component.ts
+++ app/src/app/component/user-profile/user-profile.component.ts
@@ -1,113 +1,266 @@
-import { PlatformHelper } from "src/app/shared/helper/platform.helper";
-import { Component, OnInit, OnDestroy, Input, Injector, ViewChild } from "@angular/core";
-import { BaseFormComponent } from "../base/base-form.component";
-import { UserModel } from "src/app/shared/model/user.model";
-import { ModalData } from "src/app/shared/model/modal-data.model";
-import { AlertService } from "src/app/shared/service/alert.service";
-import { UserService } from "src/app/shared/service/user.service";
-import { LoadingService } from "src/app/shared/service/loading.service";
-import { IonSlides, NavParams } from "@ionic/angular";
-import { LocalStorageService } from "src/app/shared/service/local-storage.service";
-import { BaseModel } from "src/app/shared/model/base.model";
-import { takeUntil } from "rxjs/operators";
-import { OperationsEnum } from "src/app/shared/enum/operations.enum";
+/**
+ * © Rolling Array https://rollingarray.co.in/
+ *
+ * @summary User profile component
+ * @author code@rollingarray.co.in
+ *
+ * Created at     : 2022-09-20 08:43:51 
+ * Last modified  : 2022-09-20 08:44:10
+ */
+
+
+import
+	{
+		Component,
+		OnInit,
+		OnDestroy,
+		Input,
+		ViewChild,
+		Injector,
+	} from '@angular/core';
+import { IonSlides, NavParams } from '@ionic/angular';
+import { Observable } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { OperationsEnum } from 'src/app/shared/enum/operations.enum';
+import { ModalData } from 'src/app/shared/model/modal-data.model';
+import { UserModel } from 'src/app/shared/model/user.model';
+import { AlertService } from 'src/app/shared/service/alert.service';
+import { RootStateFacade } from 'src/app/state/root/root.state.facade';
+import { BaseFormComponent } from '../base/base-form.component';
 
 @Component({
-	selector: "app-user-profile",
-	templateUrl: "./user-profile.component.html",
-	styleUrls: ["./user-profile.component.scss"],
+	selector: 'app-user-profile',
+	templateUrl: './user-profile.component.html',
+	styleUrls: ['./user-profile.component.scss'],
 })
-export class UserProfileComponent extends BaseFormComponent
+export class UserProfileComponent
+	extends BaseFormComponent
 	implements OnInit, OnDestroy
 {
-	@Input() data: string;
-	userModel: UserModel;
-	modalData: ModalData;
-	ifFormActive: boolean;
-	isApp = true;
 	/**
-	 * @description Segment  of skill requirement page
+	 * -------------------------------------------------|
+	 * @description                                     |
+	 * @readonly properties                             |
+	 * -------------------------------------------------|
 	 */
-	 private _segment!: number;
+
+	/**
+	 * -------------------------------------------------|
+	 * @description                                     |
+	 * @private Instance variable                       |
+	 * -------------------------------------------------|
+	 */
+
+	/**
+	 * Description  of user profile component
+	 */
+	private _userModel: UserModel;
+
+	/**
+	 * Modal data of user profile component
+	 */
+	private _modalData: ModalData;
+
+	/**
+	 * If form active of user profile component
+	 */
+	private _ifFormActive = false;
+
+	/**
+	 * Segment  of user profile component
+	 */
+	private _segment!: number;
+
+	/**
+	 * -------------------------------------------------|
+	 * @description                                     |
+	 * @public Instance variable                        |
+	 * -------------------------------------------------|
+	 */
+
+	/**
+	 * Description  of user profile component
+	 */
+	public modalLoadingIndicatorStatus$: Observable<boolean>;
+
+	/**
+	 * Logged in user$ of user profile component
+	 */
+	public loggedInUser$: Observable<UserModel>;
+
+	/**
+	 * -------------------------------------------------|
+	 * @description                                     |
+	 * Getter & Setters                                 |
+	 * -------------------------------------------------|
+	 */
+
+	//get user email
+	get userFirstName()
+	{
+		return this.formGroup.get('userFirstName');
+	}
+
+	get userLastName()
+	{
+		return this.formGroup.get('userLastName');
+	}
+
+	get userEmail()
+	{
+		return this.formGroup.get('userEmail');
+	}
+
+	get userSkills()
+	{
+		return this.formGroup.get('userSkills');
+	}
 
 	/**
 	 * @description Gets segment
 	 */
-	 public get segment(): number {
+	public get segment(): number
+	{
 		return this._segment;
-	 }
-	
-	 public set segment(value: number) {
+	}
+
+	/**
+	 * Sets segment
+	 */
+	public set segment(value: number)
+	{
 		this._segment = value;
 	}
-	
+
+	/**
+	 * Gets if form active
+	 */
+	public get ifFormActive()
+	{
+		return this._ifFormActive;
+	}
+
+	/**
+	 * -------------------------------------------------|
+	 * @description										|
+	 * @ViewChild Instance variable						|
+	 * -------------------------------------------------|
+	 */
+
+	/**
+	 * Description  of user profile component
+	 */
 	@ViewChild('slides', { static: false }) slides: IonSlides;
-	
+
+	/**
+	 * -------------------------------------------------|
+	 * @description                                     |
+	 * Life cycle hook                                  |
+	 * -------------------------------------------------|
+	 */
+
+	/**
+	 * Creates an instance of user profile component.
+	 * @param injector
+	 * @param alertService
+	 * @param rootStateFacade
+	 * @param navParams
+	 */
 	constructor(
 		injector: Injector,
 		private alertService: AlertService,
-		private userService: UserService,
-		private loadingService: LoadingService,
-		public navParams: NavParams,
-		private localStorageService: LocalStorageService,
-		private platformHelper: PlatformHelper,
-		//private avatarService: AvatarService
+		private rootStateFacade: RootStateFacade,
+		public navParams: NavParams
 	)
 	{
 		super(injector);
-		this.ifFormActive = false;
+		this.modalLoadingIndicatorStatus$ =
+			this.rootStateFacade.modalLoadingIndicatorStatus$;
+		this.loggedInUser$ = this.rootStateFacade.loggedInUser$;
+		this._ifFormActive = false;
 		this.getActiveUser();
 		this.buildFrom();
-		this.isApp = this.platformHelper.isApp();
 	}
 
+	/**
+	 * on init
+	 */
+	ngOnInit()
+	{
+		this._segment = 0;
+	}
+
+	/**
+	 * on destroy
+	 */
+	ngOnDestroy()
+	{
+		super.ngOnDestroy();
+	}
+
+	/**
+	 * -------------------------------------------------|
+	 * @description                                     |
+	 * @Private methods                                 |
+	 * -------------------------------------------------|
+	 */
+
+	/**
+	 * Descriptions user profile component
+	 */
 	private getActiveUser()
 	{
-		this.localStorageService
-			.getActiveUser()
+		this.loggedInUser$
 			.pipe(takeUntil(this.unsubscribe))
 			.subscribe((data: UserModel) =>
 			{
-				this.userModel = data;
+				this._userModel = data;
 			});
 	}
 
+	/**
+	 * Sets passed value to from
+	 */
 	private setPassedValueToFrom()
 	{
 		const form = this.formGroup.value;
 
-		form.userFirstName = this.userModel.userFirstName;
-		form.userLastName = this.userModel.userLastName;
-		form.userEmail = this.userModel.userEmail;
+		form.userFirstName = this._userModel.userFirstName;
+		form.userLastName = this._userModel.userLastName;
+		form.userEmail = this._userModel.userEmail;
 
 		this.formGroup.disable();
 	}
+
+	/**
+	 * Builds from
+	 */
 	private buildFrom()
 	{
 		this.formGroup = this.formBuilder.group({
 			userFirstName: [
-				this.userModel.userFirstName,
+				this._userModel.userFirstName,
 				this.validators().compose([
 					this.validators().required,
 					this.validators().pattern(this.regex.USER_NAME_PATTERN),
 				]),
 			],
 			userLastName: [
-				this.userModel.userLastName,
+				this._userModel.userLastName,
 				this.validators().compose([
 					this.validators().required,
 					this.validators().pattern(this.regex.USER_NAME_PATTERN),
 				]),
 			],
 			userEmail: [
-				this.userModel.userEmail,
+				this._userModel.userEmail,
 				this.validators().compose([
 					this.validators().required,
 					this.validators().pattern(this.regex.EMAIL_PATTERN),
 				]),
 			],
 			userSkills: [
-				this.userModel.userSkills,
+				this._userModel.userSkills,
 				this.validators().compose([
 					//
 				]),
@@ -117,35 +270,69 @@ export class UserProfileComponent extends BaseFormComponent
 		this.setPassedValueToFrom();
 	}
 
-	//get user email
-	get userFirstName()
+	/**
+	 * Builds data model to pass
+	 * @returns
+	 */
+	private buildDataModelToPass()
 	{
-		return this.formGroup.get("userFirstName");
+		// build data
+		const form = this.formGroup.value;
+		const model: UserModel = {
+			userFirstName: form.userFirstName,
+			userLastName: form.userLastName,
+			userEmail: form.userEmail,
+			userSkills: form.userSkills,
+			operationType: OperationsEnum.EDIT,
+		};
+
+		return model;
 	}
 
-	get userLastName()
+	/**
+	 * Submits data
+	 */
+	private async submitData()
 	{
-		return this.formGroup.get("userLastName");
+		//start loader
+		this.rootStateFacade.startModalLoading();
+
+		//build pass post data
+		const model: UserModel = this.buildDataModelToPass();
+
+		this.rootStateFacade.userProfileUpdate(model);
+
+		this._ifFormActive = false;
 	}
 
-	get userEmail()
+	/**
+	 * Dismiss modal
+	 */
+	private dismissModal()
 	{
-		return this.formGroup.get("userEmail");
+		this.modalController.dismiss(this._modalData).then(() => { });
 	}
 
-	get userSkills()
-	{
-		return this.formGroup.get("userSkills");
-	}
+	/**
+	 * -------------------------------------------------|
+	 * @description                                     |
+	 * @Public methods                                  |
+	 * -------------------------------------------------|
+	 */
 
-	async edit()
+	/**
+	 * Descriptions user profile component
+	 */
+	public async edit()
 	{
 		this.formGroup.enable();
-		this.ifFormActive = true;
+		this._ifFormActive = true;
 	}
 
-	// submit login
-	async submit()
+	/**
+	 * Submits user profile component
+	 */
+	public async submit()
 	{
 		if (this.formGroup.invalid)
 		{
@@ -158,117 +345,17 @@ export class UserProfileComponent extends BaseFormComponent
 		}
 	}
 
-	async submitData()
+	/**
+	 * Closes modal
+	 */
+	public closeModal()
 	{
-		//start loader
-		this.loadingService.present(`${this.stringKey.API_REQUEST_MESSAGE_2}`);
-
-		//build pass post data
-		const form = this.formGroup.value;
-		this.userModel.userFirstName = form.userFirstName;
-		this.userModel.userLastName = form.userLastName;
-		this.userModel.userEmail = form.userEmail;
-		this.userModel.userSkills = form.userSkills;
-		this.userModel.operationType = OperationsEnum.EDIT
-
-		//send api response
-		this.userService
-			.userProfileUpdate(this.userModel)
-			.pipe(takeUntil(this.unsubscribe))
-			.subscribe(
-				async (baseModel: BaseModel) =>
-				{
-					//stop loading
-					await this.loadingService.dismiss();
-
-					//check if success response case back
-					if (baseModel.success)
-					{
-						await this.localStorageService
-							.updateActiveUserDetails(this.userModel)
-							.pipe(takeUntil(this.unsubscribe))
-							.subscribe(async () =>
-							{
-								//model response when modal close
-								this.modalData = {
-									cancelled: false,
-									operationSubmitted: true,
-								};
-
-								//toast api response
-								await this.presentToast(baseModel.message);
-
-								// store active user
-								this.dismissModal();
-
-								this.formGroup.enable();
-								this.ifFormActive = true;
-							});
-					}
-				},
-				(error) =>
-				{
-					this.loadingService.dismiss();
-				}
-			);
-	}
-
-	closeModal()
-	{
-		this.modalData = {
+		this._modalData = {
 			cancelled: true,
 			operationSubmitted: false,
 		};
 
 		// store active user
 		this.dismissModal();
-	}
-
-	dismissModal()
-	{
-		this.modalController.dismiss(this.modalData).then(() => { });
-	}
-
-	ngOnInit()
-	{
-		this._segment = 0;
-	}
-
-	ngOnDestroy()
-	{
-		super.ngOnDestroy();
-	}
-
-	/**
-	 * @description Segments changed
-	 * @param event 
-	 */
-	// tslint:disable:no-any
-	segmentChanged(event: any) {
-		//this._hasRequirementDetailsData = false;
-
-		//this.slides.slideTo(ev.detail.value);
-		switch (event.detail.value) {
-			case '0':
-				// this.myAppliedRequirements();
-				// this._showAddSkillRequirement = false;
-				break;
-			case '1':
-				// this.openRequirements();
-				// this._showAddSkillRequirement = false;
-				break;
-			case '2':
-				// this.myRequirements();
-				// this._showAddSkillRequirement = true;
-				break;
-			default:
-				break;
-		}
-		//this.slideTo();
-	}
-
-	onSlideChanged()
-	{
-		
 	}
 }
