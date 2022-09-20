07ca19fc0ff2b82d79a25921cfafc5f5a7c02994
Rolling Array <82917132+RollingArray@users.noreply.github.com>
2022-09-20 12:16:47 +0530

    APP: Remove local storage dependency from sign up page

--- app/src/app/pages/sign-up/sign-up.page.ts
+++ app/src/app/pages/sign-up/sign-up.page.ts
@@ -5,20 +5,14 @@
  * @summary Sign up page
  * @author code@rollingarray.co.in
  *
- * Created at     : 2021-12-26 11:17:44 
- * Last modified  : 2022-08-02 20:28:37
+ * Created at     : 2021-12-26 11:17:44
+ * Last modified  : 2022-09-20 12:16:06
  */
-
 
 import { Component, OnInit, OnDestroy, Injector } from '@angular/core';
 import { BaseFormComponent } from 'src/app/component/base/base-form.component';
 import { AlertService } from 'src/app/shared/service/alert.service';
-import { LoadingService } from 'src/app/shared/service/loading.service';
-import { UserService } from 'src/app/shared/service/user.service';
-import { Router } from '@angular/router';
-import { BaseModel } from 'src/app/shared/model/base.model';
 import { takeUntil } from 'rxjs/operators';
-import { LocalStorageService } from 'src/app/shared/service/local-storage.service';
 import { AnalyticsService } from 'src/app/shared/service/analytics.service';
 import { EventPageEnum } from 'src/app/shared/enum/event-page.enum';
 import { AccountVerificationComponent } from 'src/app/component/account-verification/account-verification.component';
@@ -28,127 +22,178 @@ import { TranslateService } from '@ngx-translate/core';
 import { RootStateFacade } from 'src/app/state/root/root.state.facade';
 import { OperationsEnum } from 'src/app/shared/enum/operations.enum';
 
-
 @Component({
-	selector: "app-sign-up",
-	templateUrl: "./sign-up.page.html",
-	styleUrls: ["./sign-up.page.scss"],
+	selector: 'app-sign-up',
+	templateUrl: './sign-up.page.html',
+	styleUrls: ['./sign-up.page.scss'],
 })
-export class SignUpPage extends BaseFormComponent implements OnInit, OnDestroy {
-	
+export class SignUpPage extends BaseFormComponent implements OnInit, OnDestroy
+{
+	/**
+	 * -------------------------------------------------|
+	 * @description                                     |
+	 * @readonly properties                             |
+	 * -------------------------------------------------|
+	 */
+
+	/**
+	 * Description  of sign up page
+	 */
 	readonly userTypeEnum = UserTypeEnum;
-	
+
+	/**
+	 * -------------------------------------------------|
+	 * @description                                     |
+	 * @private Instance variable                       |
+	 * -------------------------------------------------|
+	 */
+
+	/**
+	 * -------------------------------------------------|
+	 * @description                                     |
+	 * @public Instance variable                        |
+	 * -------------------------------------------------|
+	 */
+
+	/**
+	 * -------------------------------------------------|
+	 * @description                                     |
+	 * Getter & Setters                                 |
+	 * -------------------------------------------------|
+	 */
+
+	/**
+	 * Gets user first name
+	 */
+	get userFirstName()
+	{
+		return this.formGroup.get('userFirstName');
+	}
+
+	/**
+	 * Gets user last name
+	 */
+	get userLastName()
+	{
+		return this.formGroup.get('userLastName');
+	}
+
+	/**
+	 * Gets user email
+	 */
+	get userEmail()
+	{
+		return this.formGroup.get('userEmail');
+	}
+
+	/**
+	 * -------------------------------------------------|
+	 * @description                                     |
+	 * @ViewChild Instance variable                     |
+	 * -------------------------------------------------|
+	 */
+
+	/**
+	 * -------------------------------------------------|
+	 * @description                                     |
+	 * Life cycle hook                                  |
+	 * -------------------------------------------------|
+	 */
+
 	/**
 	 * Creates an instance of sign up page.
-	 * @param injector 
-	 * @param alertService 
-	 * @param loadingService 
-	 * @param userService 
-	 * @param router 
+	 * @param injector
+	 * @param alertService
+	 * @param loadingService
+	 * @param userService
+	 * @param router
 	 */
 	constructor(
 		injector: Injector,
 		private alertService: AlertService,
-		private loadingService: LoadingService,
-		private userService: UserService,
-		private router: Router,
-		private localStorageService: LocalStorageService,
 		private analyticsService: AnalyticsService,
 		private rootStateFacade: RootStateFacade,
 		private translateService: TranslateService
-	) {
+	)
+	{
 		super(injector);
 		this.buildFrom();
 
 		/*
 		Log event
 		*/
-		this.analyticsService.log('', EventPageEnum.SIGN_UP,
-			{
-				'data': ''
-			}
-		);
+		this.analyticsService.log('', EventPageEnum.SIGN_UP, {
+			data: '',
+		});
 	}
 
 	/**
-	 * 
+	 * on init
 	 */
-	private buildFrom() {
+	ngOnInit() { }
+
+	/**
+	 * on destroy
+	 */
+	ngOnDestroy()
+	{
+		super.ngOnDestroy();
+	}
+
+	/**
+	 * -------------------------------------------------|
+	 * @description                                     |
+	 * @Private methods                                 |
+	 * -------------------------------------------------|
+	 */
+
+	/**
+	 * Descriptions sign up page
+	 */
+	private buildFrom()
+	{
 		this.formGroup = this.formBuilder.group({
 			userFirstName: [
-				"",
+				'',
 				this.validators().compose([
 					this.validators().required,
 					this.validators().pattern(this.regex.USER_NAME_PATTERN),
 				]),
 			],
 			userLastName: [
-				"",
+				'',
 				this.validators().compose([
 					this.validators().required,
 					this.validators().pattern(this.regex.USER_NAME_PATTERN),
 				]),
 			],
 			userEmail: [
-				"",
+				'',
 				this.validators().compose([
 					this.validators().required,
 					this.validators().pattern(this.regex.EMAIL_PATTERN),
 				]),
-			]
+			],
 		});
 
 		this.setFormData();
 	}
 
 	/**
-	 * 
+	 * Sets form data
 	 */
-	setFormData() {
+	private setFormData()
+	{
 		const form = this.formGroup.value;
-		form.userFirstName = "";
-		form.userLastName = "";
-		form.userEmail = "";
-	}
-
-	/**
-	 * Gets user first name
-	 */
-	get userFirstName() {
-		return this.formGroup.get("userFirstName");
-	}
-
-	/**
-	 * Gets user last name
-	 */
-	get userLastName() {
-		return this.formGroup.get("userLastName");
-	}
-
-	/**
-	 * Gets user email
-	 */
-	get userEmail() {
-		return this.formGroup.get("userEmail");
-	}
-
-	/**
-	 * Submits sign up page
-	 */
-	async submit(userTypeEnum: UserTypeEnum) {
-		if (this.formGroup.invalid) {
-			await this.alertService.presentBasicAlert(
-				`${this.stringKey.MANDATORY_FIELDS}`
-			);
-		} else {
-			await this.submitData(userTypeEnum);
-		}
+		form.userFirstName = '';
+		form.userLastName = '';
+		form.userEmail = '';
 	}
 
 	/**
 	 * Submits data
 	 */
-	async submitData(userTypeEnum: UserTypeEnum)
+	private async submitData(userTypeEnum: UserTypeEnum)
 	{
 		// build data userModel
 		const form = this.formGroup.value;
@@ -156,7 +201,7 @@ export class SignUpPage extends BaseFormComponent implements OnInit, OnDestroy {
 			userFirstName: form.userFirstName,
 			userLastName: form.userLastName,
 			userEmail: form.userEmail,
-			userType: userTypeEnum
+			userType: userTypeEnum,
 		};
 
 		// loader
@@ -172,10 +217,9 @@ export class SignUpPage extends BaseFormComponent implements OnInit, OnDestroy {
 		this.rootStateFacade.signUp(userModel);
 
 		// track user status
-		this.rootStateFacade
-			.selectUserLoggedInStatus$
+		this.rootStateFacade.selectUserLoggedInStatus$
 			.pipe(takeUntil(this.unsubscribe))
-			.subscribe(status =>
+			.subscribe((status) =>
 			{
 				if (status === OperationsEnum.SIGNED_UP)
 				{
@@ -185,19 +229,12 @@ export class SignUpPage extends BaseFormComponent implements OnInit, OnDestroy {
 	}
 
 	/**
-	 * on init
+	 * Loads account verification
+	 * @param userModel 
+	 * @returns  
 	 */
-	ngOnInit() { }
-
-	/**
-	 * on destroy
-	 */
-	ngOnDestroy() {
-		super.ngOnDestroy();
-	}
-
-	// add Community
-	async loadAccountVerification(userModel: UserModel) {
+	private async loadAccountVerification(userModel: UserModel)
+	{
 		const modal = await this.modalController.create({
 			component: AccountVerificationComponent,
 			componentProps: {
@@ -205,12 +242,34 @@ export class SignUpPage extends BaseFormComponent implements OnInit, OnDestroy {
 			},
 		});
 
-		modal.onDidDismiss().then((data) => {
+		modal.onDidDismiss().then((data) =>
+		{
 			//if app, initiate push notificaiton
-			
 		});
 
 		return await modal.present();
+	}
 
+	/**
+	 * -------------------------------------------------|
+	 * @description                                     |
+	 * @Public methods                                  |
+	 * -------------------------------------------------|
+	 */
+
+	/**
+	 * Submits sign up page
+	 */
+	public async submit(userTypeEnum: UserTypeEnum)
+	{
+		if (this.formGroup.invalid)
+		{
+			await this.alertService.presentBasicAlert(
+				`${this.stringKey.MANDATORY_FIELDS}`
+			);
+		} else
+		{
+			await this.submitData(userTypeEnum);
+		}
 	}
 }
