aceeb9cd689a3add42f3fa3ac82e35ff736006fd
Rolling Array <82917132+RollingArray@users.noreply.github.com>
2022-09-21 14:35:17 +0530

    ADD: Update panel info to accept info content as more or less visibility

--- app/src/app/component/panel-info/panel-info.component.module.ts
+++ app/src/app/component/panel-info/panel-info.component.module.ts
@@ -5,16 +5,17 @@
  * @author code@rollingarray.co.in
  *
  * Created at     : 2022-09-13 09:45:15 
- * Last modified  : 2022-09-13 09:46:02
+ * Last modified  : 2022-09-21 14:21:51
  */
 
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { PanelInfoComponent } from './panel-info.component';
+import { I18nModule } from 'src/app/shared/module/i18n.module';
 
 @NgModule({
-  imports: [CommonModule, IonicModule],
+  imports: [CommonModule, IonicModule, I18nModule],
 
   declarations: [PanelInfoComponent],
   exports: [PanelInfoComponent],
--- app/src/app/component/panel-info/panel-info.component.ts
+++ app/src/app/component/panel-info/panel-info.component.ts
@@ -4,32 +4,149 @@
  * @summary Panel info component
  * @author code@rollingarray.co.in
  *
- * Created at     : 2022-09-13 09:45:15 
- * Last modified  : 2022-09-15 20:43:18
+ * Created at     : 2022-09-13 09:45:15
+ * Last modified  : 2022-09-21 14:28:51
  */
 
 import { BaseViewComponent } from 'src/app/component/base/base-view.component';
 import { Component, OnInit, Input, Injector } from '@angular/core';
+import { StringKey } from 'src/app/shared/constant/string.constant';
 
 @Component({
-  selector: 'panel-info',
-  templateUrl: './panel-info.component.html',
-  styleUrls: ['./panel-info.component.scss'],
+	selector: 'panel-info',
+	templateUrl: './panel-info.component.html',
+	styleUrls: ['./panel-info.component.scss'],
 })
 export class PanelInfoComponent extends BaseViewComponent implements OnInit {
+	/**
+	 * -------------------------------------------------|
+	 * @description                                     |
+	 * @readonly properties                             |
+	 * -------------------------------------------------|
+	 */
 
-  @Input() title = '';
-  @Input() panelColor = '';
-  @Input() panelIcon;
-  @Input() fontClass = 'font-size-15-px';
-  @Input() fontColor = 'medium';
-  
-  constructor(
-    injector: Injector,
-  ) { 
-    super(injector);
-  }
+	/**
+	 * -------------------------------------------------|
+	 * @description                                     |
+	 * @private Instance variable                       |
+	 * -------------------------------------------------|
+	 */
 
-  ngOnInit() {}
+	/**
+	 * Description  of panel info component
+	 */
+	private _showMore = true;
 
+	/**
+	 * -------------------------------------------------|
+	 * @description                                     |
+	 * @public Instance variable                        |
+	 * -------------------------------------------------|
+	 */
+
+	/**
+	 * -------------------------------------------------|
+	 * @description										|
+	 * @input & @output Instance variable				|
+	 * -------------------------------------------------|
+	 */
+
+	/**
+	 * Description  of panel info component
+	 */
+	@Input() title = '';
+
+	/**
+	 * Input  of panel info component
+	 */
+	@Input() panelColor = '';
+
+	/**
+	 * Input  of panel info component
+	 */
+	@Input() panelIcon = StringKey.ICON_INFO;
+
+	/**
+	 * Input  of panel info component
+	 */
+	@Input() fontClass = 'font-size-15-px';
+
+	/**
+	 * Input  of panel info component
+	 */
+	@Input() fontColor = 'medium';
+
+	/**
+	 * Input  of panel info component
+	 */
+	@Input() showMoreLess = false;
+
+	/**
+	 * -------------------------------------------------|
+	 * @description                                     |
+	 * Getter & Setters                                 |
+	 * -------------------------------------------------|
+	 */
+
+	/**
+	 * -------------------------------------------------|
+	 * @description                                     |
+	 * Getter & Setters                                 |
+	 * -------------------------------------------------|
+	 */
+
+	/**
+	 * Gets description
+	 */
+	get showMore() {
+		return this._showMore;
+	}
+
+	/**
+	 * -------------------------------------------------|
+	 * @description                                     |
+	 * @ViewChild Instance variable                     |
+	 * -------------------------------------------------|
+	 */
+
+	/**
+	 * -------------------------------------------------|
+	 * @description                                     |
+	 * Life cycle hook                                  |
+	 * -------------------------------------------------|
+	 */
+
+	/**
+	 * Creates an instance of panel info component.
+	 * @param injector
+	 */
+	constructor(injector: Injector) {
+		super(injector);
+	}
+
+	/**
+	 * on init
+	 */
+	ngOnInit() {}
+
+	/**
+	 * -------------------------------------------------|
+	 * @description                                     |
+	 * @Private methods                                 |
+	 * -------------------------------------------------|
+	 */
+
+	/**
+	 * -------------------------------------------------|
+	 * @description                                     |
+	 * @Public methods                                  |
+	 * -------------------------------------------------|
+	 */
+
+	/**
+	 * Descriptions panel info component
+	 */
+	toggleContentView() {
+		this._showMore = !this._showMore;
+	}
 }
