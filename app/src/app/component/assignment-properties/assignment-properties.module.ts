3b2de6599c0a8c44bce86818c8f48e60e8323b2a
Rolling Array <82917132+RollingArray@users.noreply.github.com>
2022-09-22 16:56:46 +0530

    APP: Modify assignment properties look

--- app/src/app/component/assignment-properties/assignment-properties.module.ts
+++ app/src/app/component/assignment-properties/assignment-properties.module.ts
@@ -6,7 +6,7 @@
  * @author code@rollingarray.co.in
  *
  * Created at     : 2022-07-27 18:49:32 
- * Last modified  : 2022-07-28 09:49:48
+ * Last modified  : 2022-09-22 16:31:24
  */
 
 import { CommonModule } from "@angular/common";
@@ -16,6 +16,8 @@ import { IonicModule } from "@ionic/angular";
 import { TranslateModule } from "@ngx-translate/core";
 import { SharedModule } from "src/app/shared/module/shared.module";
 import { CourseMaterialMenuStateModule } from "src/app/state/course-material-menu/course-material-menu.state.module";
+import { PanelHeaderModule } from "../panel-header/panel-header.component.module";
+import { PanelInfoModule } from "../panel-info/panel-info.component.module";
 import { AssignmentPropertiesComponent } from "./assignment-properties.component";
 
 @NgModule({
@@ -26,6 +28,8 @@ import { AssignmentPropertiesComponent } from "./assignment-properties.component
 		TranslateModule,
 		SharedModule,
 		CourseMaterialMenuStateModule,
+		PanelHeaderModule,
+		PanelInfoModule
 	],
 
 	declarations: [AssignmentPropertiesComponent],
